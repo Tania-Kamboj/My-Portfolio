e37712a0a363af768ccb5f7890cd79614fce96bb
Tania Kamboj <taniapmvt@gmail.com>
2026-08-12 13:30:37 +0530

    fixed merge conflict in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
-  return <footer className="footer">© 2026 Tania · Built with &#128156; in React + GSAP ·</footer>
-<<<<<<< HEAD
+  return (
+    <footer className="footer">
+      &#169 2026 Tania · Built with &#128156; in React + GSAP ·
+    </footer>
+  )
 }
-=======
-}
->>>>>>> 26022c90813e8ad3895c60b40c43a1a46e7cda70
